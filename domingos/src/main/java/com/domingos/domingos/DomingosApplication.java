2a4d449ce0be9d722255b14748a14c7bf51a80d8
domingosChaves <dudu.dekst18@gmail.com>
2022-09-13 15:43:56 -0300

    Inicio dos estudos

---
+++ domingos/src/main/java/com/domingos/domingos/DomingosApplication.java
@@ -0,0 +1,13 @@
+package com.domingos.domingos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomingosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomingosApplication.class, args);
+	}
+
+}
